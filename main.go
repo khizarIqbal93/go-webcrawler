66d32eb920612e918edbc57e62393578ce1e02f1
Khizar Iqbal <khizar.iqbal@ecs.co.uk>
2022-03-04 23:51:43 +0000

    function name changed to aTagLinkMatcher and updated to find only hrefs in a tags which then extracts links

--- main.go
+++ main.go
@@ -12,14 +12,18 @@ func main() {
 	fmt.Println("Enter a url")
 	fmt.Scanln(&url)
 	html := getHtml(url)
-	links, numOfLinks := htmlLinkMatcher(html)
-	fmt.Printf("This page has %v links\n", numOfLinks)
-	fmt.Println("here are all the links>>>>>>\n", links)
+	links, numOfLinks := aTagLinkMatcher(html)
+	fmt.Printf("This page has %v links!\n", numOfLinks)
+	fmt.Println("here are all the links\n>>>>>>\n", links, "\n>>>>>>>")
 }
 
-func htmlLinkMatcher(html string) ([]string, int) {
-	r, _ := regexp.Compile(`(href=)?"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"`)
-	var links []string = r.FindAllString(html, -1)
+func aTagLinkMatcher(html string) ([]string, int) {
+	r, _ := regexp.Compile(`<a (href=)?"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)">`)
+	var aTags []string = r.FindAllString(html, -1)
+	links := []string{}
+	for i := 0; i < len(aTags); i++ {
+		links = append(links, aTags[i][9:len(aTags[i]) -2])
+	}
 	return links, len(links)
 }
 
